970597965092bccc23306291d729f7b0c2c5c76a
Saiful Anwar <saiful.bks@gmail.com>
2026-09-06 01:35:03 +0700

    update database

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
 from database import Base
 
 
@@ -9,3 +11,4 @@ class User(Base):
     name = Column(String(100), nullable=False)
     email = Column(String(255), nullable=False, unique=True)
     password_hash = Column(String(255), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
